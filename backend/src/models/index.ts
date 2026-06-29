e2232aec69a19861dc4d81f58188a89af7f60836
AltamasKausar1 <altamaska@student.iul.ac.in>
2026-06-29 19:54:35 +0530

    Deleted Backend

--- backend/src/models/index.ts
+++
@@ -1,6 +0,0 @@
-export { User, type IUser } from './User';
-export { Canteen, type ICanteen } from './Canteen';
-export { MenuItem, type IMenuItem } from './MenuItem';
-export { Order, type IOrder } from './Order';
-export { Offer, type IOffer } from './Offer';
-export { Coupon, type ICoupon } from './Coupon';
